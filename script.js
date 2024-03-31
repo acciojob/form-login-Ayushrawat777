e5b90a0771333e440e1ee75a1d484f0fdd594867
AYUSH <106034287+Ayushrawat777@users.noreply.github.com>
2024-04-01 02:51:55 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
 function getFormvalue() {
     //Write your code here
-
+var name=document.getElementById('fname').value;
+	var last=document.getElementById('lname').value;
+	alert(name+ " "+ last);
 }
